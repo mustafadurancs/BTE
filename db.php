2b46f0628b206eb6983dc51cafe1f7f7e3e26011
mduranmustafa <33037715+mduranmustafa@users.noreply.github.com>
2017-11-29 06:53:36 +0100

    silindi
    aynı klasöre yanlışlıkla konuldu

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$Sunucu="localhost";
-$Kullanici="root";
-$Sifre="";
-$VT="danisman";
-$Baglanti=  mysql_pconnect($Sunucu,$Kullanici,$Sifre) or die ("Hata!!:".mysql_error());
-mysql_query("SET NAMES UTF8");
-?>
--- kontrol.js
+++
@@ -1,9 +0,0 @@
-function validateForm(Form_Ismi) {
-    var x = document.forms[Form_Ismi]["randevu"].value;
-    if (x == null || x == "") 
-    {
-        alert("ADI ve SOYADINI GİRMEDİNİZ!!!");
-        return false;
-    }
-}
-
